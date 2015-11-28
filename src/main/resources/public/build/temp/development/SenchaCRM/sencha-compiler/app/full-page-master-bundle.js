4750ba0a6e302b09e7ede86880577b084e2b3ff6
Shinobu Kawano <kawanoshinobu@gmail.com>
2015-11-28 13:17:32 +0900

    Removed files.

--- src/main/resources/public/build/temp/development/SenchaCRM/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/kawanoshinobu/git/SenchaCRM/src/main/resources/public/app.js
--- src/main/resources/public/build/temp/production/SenchaCRM/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/Users/kawanoshinobu/git/SenchaCRM/src/main/resources/public/build/.sass-cache'
