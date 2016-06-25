44ca0f5b9172542e43726e32f4f58a150e1ebe45
Shinobu Kawano <kawanoshinobu@gmail.com>
2016-06-25 21:51:07 +0900

    Added CRUD test.

--- client/test/function/Contacts.js
+++ client/test/function/Contacts.js
@@ -1,5 +1,75 @@
-describe("Edit", function() {
+describe("レコードの基本操作", function() {
+
     beforeEach(function() {
+        Ext.first('people-detail').setCollapsed(true);
+    });
+
+    it("レコードを新規追加できること", function() {
+        ST.button('[text=\'追加\']').click().and(function () {
+            ST.textField('[reference=lastname]').focus().type('Sencha');
+            ST.textField('[reference=firstname]').focus().type('太郎');
+            ST.button('[text=\'保存\']').click().wait(500).and(function () {
+                ST.button('[text=\'OK\']').and(function (button) {
+                    expect(button).toBeDefined();
+                    button.click();
+                });
+            });
+        });
+    });
+
+    it("レコードの取得できること", function() {
+        ST.grid('people-list > gridpanel').
+        rowAt(0).and(function (row) {
+            expect(row.record.get('lastName')).toBe('Sencha');
+            expect(row.record.get('firstName')).toBe('太郎');
+        })
+    });
+
+    it("レコードを編集できること", function() {
+        ST.grid('people-list > gridpanel').
+        rowAt(0).
+        cellAt(0).
+        click().and(function () {
+            ST.textField('[reference=firstname]').setValue('花子');
+            ST.button('[text=\'保存\']').click().wait(500).and(function () {
+                ST.button('[text=\'OK\']').and(function (button) {
+                    expect(button).toBeDefined();
+                    button.click();
+                });
+            });
+        }).and(function () {
+            ST.grid('people-list > gridpanel').
+            rowAt(0).and(function (row) {
+                expect(row.record.get('lastName')).toBe('Sencha');
+                expect(row.record.get('firstName')).toBe('花子');
+            })
+        });
+    });
+
+    it("レコードを削除できること", function() {
+        ST.grid('people-list > gridpanel').
+        rowAt(0).
+        cellAt(6).
+        click().and(function () {
+            ST.button('[text=\'編集内容の保存\']').click().and(function () {
+                ST.button('[text=\'はい\']').click().wait(500).and(function () {
+                    ST.button('[text=\'OK\']').click();
+                });
+            });
+        }).and(function () {
+            ST.grid('people-list > gridpanel').
+            rowAt(0).and(function (row) {
+                expect(row.record.get('lastName')).not.toBe('Sencha');
+                expect(row.record.get('firstName')).not.toBe('花子');
+            })
+        });
+    });
+});
+
+describe("ボタンのステータス", function() {
+    beforeEach(function() {
+        Ext.first('people-detail').setCollapsed(true);
+        
         ST.grid('people-list > gridpanel').
         rowAt(0).
         cellAt(0).
